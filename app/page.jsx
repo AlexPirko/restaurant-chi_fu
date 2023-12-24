8b89a5d1c0bba405fe9ce891747ef9c41485f2e4
Alex Pirko <youzhny.foto@gmail.com>
2023-12-24 14:14:36 +0200

    add head section

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,9 @@ import StyleGuide from '@/components/StyleGuide';
 export default function Home() {
     return (
         <main className='w-full max-w-[1440px] bg-white mx-auto overflow-hidden'>
-            {/* <StyleGuide /> */}
             <Head/>
+            {/* <StyleGuide /> */}
+            <div className='h-[4000px]'></div>
         </main>
     );
 }
